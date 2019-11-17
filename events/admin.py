c081280df4ffdb27804fc789a0445cac858b790e
tworedz <nurik99kg@gmail.com>
2019-11-18 01:24:59 +0600

    Add room to admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from events.models import Event, Book
+from events.models import Event, Book, Room, Choice
 
 admin.site.register(Event)
 admin.site.register(Book)
+admin.site.register(Room)
+admin.site.register(Choice)
