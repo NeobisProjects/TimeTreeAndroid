02feec15207fa19480a98b148fd209dc5e660efd
tworedz <nurik99kg@gmail.com>
2019-12-22 02:55:46 +0600

    remove default message as ru

--- configs/settings.py
+++ configs/settings.py
@@ -152,8 +152,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-# LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
 
 TIME_ZONE = config('TIME_ZONE')
 
