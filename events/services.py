c552bcee7e91ca9c275b1522af235ca34d46669f
tworedz <nurik99kg@gmail.com>
2019-12-22 02:24:37 +0600

    fix

--- events/services.py
+++ events/services.py
@@ -17,7 +17,7 @@ class Notifier:
         title = _('Event created')
         choices = Choice.objects.filter(choice=constants.CONFUSED)
         for choice in choices:
-            body = _('You didn\'t reply to this event: %s' % choice.event)
+            body = _('You didn\'t reply to this event: %s' % str(choice.event))
             Notifier.format_message(choice.user, title, body)
 
     @staticmethod
